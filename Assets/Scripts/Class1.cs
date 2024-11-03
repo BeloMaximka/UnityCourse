aea41dd79964535277b1707f4f64f4d508da8b9b
BeloMaximka <belomaxim@gmail.com>
2024-11-03 13:43:31 +0200

    Added project files

---
+++ Assets/Scripts/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+///TEЫЫЫЫЫЫЫ
+namespace Assets.Scripts
+{
+    internal class Class1
+    {
+    }
+}
